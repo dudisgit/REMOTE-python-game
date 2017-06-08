303d27c63f681795a57cd2970c753eff3e203a97
dudisgit <dudethisbotguyisedgy@gmail.com>
2017-06-08 15:24:55 +0100

    Basic pygame loop

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import pygame
 pygame.init()
+
+FPS = 60
+RESLUTION = [800,500]
+
+
+main = pygame.display.set_mode(RESLUTION)
+pygame.display.set_caption("REMOTE")
+clock = pygame.time.Clock()
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    main.fill((0,0,0))
+    
+    pygame.display.flip()
+    clock.tick(FPS)
+pygame.quit()
